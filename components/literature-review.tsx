197f33c0ff49dae95988eeae9d82afffe50c411c
arnau <aag84@bath.ac.uk>
2024-04-14 11:33:24 +0100

    psuhinig button

--- components/literature-review.tsx
+++ components/literature-review.tsx
@@ -4,6 +4,8 @@ import React, { useEffect } from 'react'
 import { PartialLiteratureReview } from '@/lib/schema/literature-review'
 import { useStreamableValue } from 'ai/rsc'
 import { MemoizedReactMarkdown } from './ui/markdown'
+import { Button } from './ui/button'
+import { DownloadIcon } from 'lucide-react'
 
 export type LiteratureReviewProps = {
   literatureReview?: PartialLiteratureReview
@@ -22,6 +24,10 @@ export const LiteratureReview: React.FC<LiteratureReviewProps> = ({ literatureRe
       <div className="mt-4">
         <MemoizedReactMarkdown className="prose-sm prose-neutral prose-a:text-accent-foreground/50">{data?.content}</MemoizedReactMarkdown>
       </div>
+    <Button variant={"outline"} className='mt-4'>
+      Download LaTeX
+      <DownloadIcon size={16} className="inline-block ml-2" />
+    </Button>
     </div>
   );
 }
